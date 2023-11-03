00acc1d1d7d5c6775b59aee2d32ade983324fa2b
Atn4s <61942303+Atn4s@users.noreply.github.com>
2023-11-02 23:36:52 -0300

    Fix: usuário comum não cadastra e mantem token!

--- Back-end/servidor.py
+++ Back-end/servidor.py
@@ -8,6 +8,11 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 from functools import wraps
 import Tables
+import logging
+import traceback
+
+# Definir o nível de log para depuração
+logging.basicConfig(level=logging.DEBUG)
 
 # Configurações do aplicativo Flask
 app = Flask('SADPPBV')
@@ -25,6 +30,23 @@ jwt = JWTManager(app)
 # Inicializar base de dados (Tables.py)
 Tables.initialize_database()
 
+#decodificar token
+
+def decode_token(encoded_token, csrf_value, allow_expired=False):
+    try:
+        token_parts = encoded_token.split(".")
+        if len(token_parts) != 3:
+            raise jwt.exceptions.InvalidTokenError("Token inválido: formato incorreto")
+        
+        return jwt.decode(encoded_token, app.config['JWT_SECRET_KEY'], algorithms=["HS256"], options={"verify_signature": True})
+    except jwt.ExpiredSignatureError:
+        if allow_expired:
+            return jwt.decode(encoded_token, app.config['JWT_SECRET_KEY'], algorithms=["HS256"], options={"verify_signature": False})
+        raise
+    except jwt.InvalidTokenError:
+        logging.debug("Token inválido")
+        raise
+
 # Autenticação do usuário!                                                                              
 def authenticate_user(registro, senha_hash_blake2b):
     conn = sqlite3.connect('project_data.db')
@@ -37,8 +59,6 @@ def authenticate_user(registro, senha_hash_blake2b):
             return {'user_id': user[0], 'tipo_usuario': user[2], 'senha': senha_hash_blake2b}
     return None
 
-
-# Verificação Token JWT
 def verify_token(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -47,14 +67,18 @@ def verify_token(f):
             if auth_header:
                 token = auth_header.split(" ")[1]
                 if token in revoked_tokens:
-                    return jsonify({'message': 'Não autenticado. Faça login novamente.', 'success': False}), 401
+                    logging.debug("Token revogado")
+                    return jsonify({'message': 'Não autenticado. Faça login novamente.', 'success': False}, 401)
                 else:
+                    logging.debug("Token válido")
                     return jwt_required()(f)(*args, **kwargs)  # Verifica se o token é válido
             else:
-                return jsonify({'message': 'Token de autenticação não encontrado', 'success': False}), 401
+                logging.debug("Token de autenticação não encontrado")
+                return jsonify({'message': 'Token de autenticação não encontrado', 'success': False}, 401)
         except Exception as e:
-            print(e)
-            return jsonify({'message': 'Erro ao processar a solicitação', 'success': False}), 500
+            logging.error(f"Erro: {e}")
+            logging.error(f"Traceback: {traceback.format_exc()}")
+            return jsonify({'message': 'Erro ao processar a solicitação', 'success': False}, 500)
     return decorated_function
 
 # Login método POST
@@ -63,16 +87,23 @@ def login():
     registro = request.json.get('registro', None)
     senha = request.json.get('senha', None)
 
-    if not registro or not senha:
-        return jsonify({"success": False, "message": "Credenciais inválidas"}), 401
+    try:
+        if not registro or not senha:
+            logging.debug("Credenciais inválidas")
+            return jsonify({"success": False, "message": "Credenciais inválidas"}), 401
 
-    senha_hash_blake2b = hashlib.blake2b(senha.encode()).hexdigest()  # Criptografa a senha em MD5 novamente
-    current_user = authenticate_user(registro, senha_hash_blake2b)
-    if not current_user:
-        return jsonify({"success": False, "message": "Credenciais inválidas"}), 401
+        senha_hash_blake2b = hashlib.blake2b(senha.encode()).hexdigest()  # Criptografa a senha em MD5 novamente
+        current_user = authenticate_user(registro, senha_hash_blake2b)
+        if not current_user:
+            return jsonify({"success": False, "message": "Credenciais inválidas"}), 401
 
-    token = create_access_token(identity={'user_id': current_user['user_id'], 'tipo_usuario': current_user['tipo_usuario'], 'senha': senha_hash_blake2b, 'registro': registro}, expires_delta=ACCESS_EXPIRES)
-    return jsonify({"success": True, "message": "Login bem-sucedido", "token": token}), 200
+        token = create_access_token(identity={'user_id': current_user['user_id'], 'tipo_usuario': current_user['tipo_usuario'], 'senha': senha_hash_blake2b, 'registro': registro}, expires_delta=ACCESS_EXPIRES)
+        return jsonify({"success": True, "message": "Login bem-sucedido", "token": token}), 200
+
+    except Exception as e:
+            logging.error(f"Erro ao processar o login: {e}")
+            logging.error(f"Traceback: {traceback.format_exc()}")
+            return jsonify({"success": False, "message": "Erro ao processar a solicitação de login"}), 500
 
 # Logout método POST 
 @app.route('/logout', methods=['POST'])
@@ -87,73 +118,87 @@ def fazer_logout():
         revoked_tokens.add(token)
 
         return jsonify({"success": True, "message": "Logout bem-sucedido"}), 200
-    except:
-        return jsonify({"success": False, "message": "Não autenticado"}), 401
+    except Exception as e:
+        logging.error(f"Erro ao processar o logout: {e}")
+        logging.error(f"Traceback: {traceback.format_exc()}")
+        return jsonify({"success": False, "message": "Erro ao processar a solicitação de logout"}), 500
 
 # Usuários método POST
 @app.route('/usuarios', methods=['POST'])
 @verify_token
 def cadastrar_usuario():
     current_user = get_jwt_identity()
-    if current_user and current_user['tipo_usuario'] == 1:  # Verifica se o tipo de usuário é 1 para administrador
-        auth_header = request.headers.get('Authorization')
-        token = auth_header.split(" ")[1]  # Obtém o token do header de autorização
-        
-        new_user = request.json
-        if not new_user:
-            return jsonify({"success": False, "message": "Dados de usuário ausentes. Por favor, forneça os dados necessários."}), 400
+    try:
+        if current_user and current_user['tipo_usuario'] == 1:  # Verifica se o tipo de usuário é 1 para administrador
+            auth_header = request.headers.get('Authorization')
+            token = auth_header.split(" ")[1]  # Obtém o token do header de autorização
+            
+            new_user = request.json
+            if not new_user:
+                return jsonify({"success": False, "message": "Dados de usuário ausentes. Por favor, forneça os dados necessários."}), 400
 
-        conn = sqlite3.connect('project_data.db')
-        cursor = conn.cursor()
-        try:
-            senha_hash_blake2b = hashlib.blake2b(new_user['senha'].encode()).hexdigest()  # Criptografa a senha em MD5 novamente
-            cursor.execute("INSERT INTO usuario (nome, registro, email, senha, tipo_usuario) VALUES (?, ?, ?, ?, ?)",
-                       (new_user['nome'], new_user['registro'], new_user['email'], senha_hash_blake2b, new_user['tipo_usuario']))
-            conn.commit()
-            return jsonify({"success": True, "message": "Novo usuário cadastrado com sucesso."}), 200
-        except sqlite3.IntegrityError as e:
-            return jsonify({"success": False, "message": "O registro já está em uso. Por favor, escolha um registro diferente."}), 400
-    else:
-        return jsonify({"success": False, "message": "Acesso negado. Você não tem permissão para realizar esta ação."}), 401
+            conn = sqlite3.connect('project_data.db')
+            cursor = conn.cursor()
+            try:
+                senha_hash_blake2b = hashlib.blake2b(new_user['senha'].encode()).hexdigest()  # Criptografa a senha em MD5 novamente
+                cursor.execute("INSERT INTO usuario (nome, registro, email, senha, tipo_usuario) VALUES (?, ?, ?, ?, ?)",
+                           (new_user['nome'], new_user['registro'], new_user['email'], senha_hash_blake2b, new_user['tipo_usuario']))
+                conn.commit()
+                return jsonify({"success": True, "message": "Novo usuário cadastrado com sucesso."}), 200
+            except sqlite3.IntegrityError as e:
+                return jsonify({"success": False, "message": "O registro já está em uso. Por favor, escolha um registro diferente."}), 400
+        else:
+            return jsonify({"success": False, "message": "Acesso negado. Você não tem permissão para realizar esta ação."}), 401
+    except Exception as e:
+        logging.error(f"Erro ao processar o cadastro do usuário: {e}")
+        logging.error(f"Traceback: {traceback.format_exc()}")
+        return jsonify({"success": False, "message": "Erro ao processar a solicitação de cadastro de usuário"}), 500
 
-# Usuários método GET
 @app.route('/usuarios', methods=['GET'])
 @verify_token
 def get_usuario():
-    current_user = get_jwt_identity()
-    if current_user:
-        if 'tipo_usuario' in current_user and current_user['tipo_usuario'] == 1:
-            conn = sqlite3.connect('project_data.db')
-            cursor = conn.cursor()
-            cursor.execute("SELECT id, nome, registro, email, tipo_usuario FROM usuario")
-            data = cursor.fetchall()
-            usuarios = [{'id': row[0], 'nome': row[1], 'registro': row[2], 'email': row[3], 'tipo_usuario': row[4]} for row in data]
-            for usuario in usuarios:
-                usuario.pop('senha', None)  # Remove a senha, se existir
-            conn.close()
-            return jsonify({'usuarios': usuarios})
-        elif 'user_id' in current_user:
-            user_id = current_user['user_id']
-            conn = sqlite3.connect('project_data.db')
-            cursor = conn.cursor()
-            cursor.execute("SELECT id, nome, registro, email, tipo_usuario FROM usuario WHERE id=?", (user_id,))
-            data = cursor.fetchone()
-            usuario = {'id': data[0], 'nome': data[1], 'registro': data[2], 'email': data[3], 'tipo_usuario': data[4]}
-            conn.close()
-            return jsonify({'usuario': usuario})
+    try:
+        current_user = get_jwt_identity()
+        if current_user:
+            logging.debug("Usuário autenticado")
+            if 'tipo_usuario' in current_user and current_user['tipo_usuario'] == 1:  # Verifica se o tipo de usuário é 1 (administrador)
+                conn = sqlite3.connect('project_data.db')
+                cursor = conn.cursor()
+                cursor.execute("SELECT id, nome, registro, email, tipo_usuario FROM usuario")
+                data = cursor.fetchall()
+                usuarios = [{'id': row[0], 'nome': row[1], 'registro': row[2], 'email': row[3], 'tipo_usuario': row[4]} for row in data]
+                for usuario in usuarios:
+                    usuario.pop('senha', None)  # Remove a senha, se existir
+                conn.close()
+                return jsonify({'usuarios': usuarios})
+            else:  # Se o tipo de usuário não for 1 (administrador), retorna apenas a conta do próprio usuário
+                conn = sqlite3.connect('project_data.db')
+                cursor = conn.cursor()
+                cursor.execute("SELECT id, nome, registro, email, tipo_usuario FROM usuario WHERE registro=?", (current_user['registro'],))
+                data = cursor.fetchone()
+                if data:
+                    usuario = {'id': data[0], 'nome': data[1], 'registro': data[2], 'email': data[3], 'tipo_usuario': data[4]}
+                    conn.close()
+                    return jsonify({'usuario': usuario})
+                else:
+                    return jsonify({"success": False, "message": "O usuário não foi encontrado."}), 404
         else:
-            return jsonify({'message': 'Chave "user_id" não encontrada no objeto current_user', 'success': False}), 400
-    else:
-        return jsonify({'message': 'Não foi possível obter as informações do usuário', 'success': False}), 401
+            logging.error('Não foi possível obter as informações do usuário')
+            return jsonify({'message': 'Não foi possível obter as informações do usuário', 'success': False}), 401
+    except Exception as e:
+        logging.error(f'Erro ao processar a solicitação de obter usuário: {e}')
+        logging.error(f'Traceback: {traceback.format_exc()}')
+        return jsonify({'message': 'Erro ao processar a solicitação', 'success': False}), 500
 
 
 @app.route('/usuarios/<string:registro>', methods=['GET'])
 @verify_token
 def get_usuario_by_registro(registro):
-    current_user = get_jwt_identity()
-    if current_user:
-        try:
-            if current_user['tipo_usuario'] == 1 or current_user['registro'] == registro:
+    try:
+        current_user = get_jwt_identity()
+        if current_user:
+            if current_user['tipo_usuario'] == 1 or (current_user['tipo_usuario'] == 0 and current_user['registro'] == registro):  # Verifica se é um administrador ou se é um usuário comum acessando seus próprios dados
+                logging.debug("Verificando permissões de acesso")
                 conn = sqlite3.connect('project_data.db')
                 cursor = conn.cursor()
                 cursor.execute("SELECT id, nome, registro, email, tipo_usuario FROM usuario WHERE registro=?", (registro,))
@@ -165,12 +210,14 @@ def get_usuario_by_registro(registro):
                 else:
                     return jsonify({"success": False, "message": "O usuário com o registro especificado não foi encontrado."}), 404
             else:
-                return jsonify({"success": False, "message": "Acesso negado. Você não tem permissão para realizar esta ação."}), 401
-        except Exception as e:
-            return jsonify({'message': 'Erro ao processar a solicitação: {}'.format(str(e)), 'success': False}), 500
-    else:
-        return jsonify({"success": False, "message": "Acesso negado. Você não está autenticado."}), 401
-
+                return jsonify({"success": False, "message": "Acesso negado. Você não tem permissão para acessar esta rota."}), 401
+        else:
+            logging.error('Acesso negado. Você não está autenticado.')
+            return jsonify({"success": False, "message": "Acesso negado. Você não está autenticado."}), 401
+    except Exception as e:
+        logging.error(f'Erro ao processar a solicitação: {e}')
+        logging.error(f'Traceback: {traceback.format_exc()}')
+        return jsonify({'message': 'Erro ao processar a solicitação', 'success': False}), 500
 
 
 # Servidor Flask
